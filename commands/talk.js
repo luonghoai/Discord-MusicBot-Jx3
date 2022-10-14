cfdcd0d4d8929223332014df8cc0c6d5f5458dd9
luonghoai <nguyenluonghoai15@gmail.com>
2022-10-14 20:31:44 +0700

    add ffmeg static

--- commands/talk.js
+++ commands/talk.js
@@ -39,7 +39,7 @@ module.exports = {
       const channelId = msg.member.voice.channelID;
       const channel = client.channels.cache.get(channelId);
       channel.join().then((connection) => {
-        broadcast.play(discordTTS.getVoiceStream(cmd));
+        broadcast.play(discordTTS.getVoiceStream(cmd, {lang: 'vi', slow: false}));
         const dispatcher = connection.play(broadcast);
       });
     } catch (err) {
